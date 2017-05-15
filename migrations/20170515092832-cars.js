00198fe70b9719f17dad6e4926a521d75e9d9e9b
IK <ik@develup.pro>
2017-05-15 12:41:27 +0300

    migrations

---
+++ migrations/20170515092832-cars.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+};
+
+exports.up = function (db, cb) {
+    db.createTable('cars', {
+        id: {type: 'int', primaryKey: true},
+        header: 'string',
+        link: 'string',
+        site_id: 'smallint',
+        created_at: 'timestamp',
+        updated_at: 'timestamp',
+    }, cb);
+};
+
+exports.down = function (db, cb) {
+    db.dropTable('cars', cb);
+};
+
+exports._meta = {
+    "version": 1
+};
+
+/*module.exports = {
+ CHAR: 'char',
+ STRING: 'string',
+ TEXT: 'text',
+ SMALLINT: 'smallint',
+ BIGINT: 'bigint',
+ INTEGER: 'int',
+ SMALL_INTEGER: 'smallint',
+ BIG_INTEGER: 'bigint',
+ REAL: 'real',
+ DATE: 'date',
+ DATE_TIME: 'datetime',
+ TIME: 'time',
+ BLOB: 'blob',
+ TIMESTAMP: 'timestamp',
+ BINARY: 'binary',
+ BOOLEAN: 'boolean',
+ DECIMAL: 'decimal'
+ };*/
